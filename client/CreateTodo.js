33bec41eab32d2fc9ac63d3cde74c761e505a518
Maria Obaturova <m.obaturova@gmail.com>
2019-05-07 11:59:32 -0400

    Creating a TODO (axios.post)

--- client/CreateTodo.js
+++ client/CreateTodo.js
@@ -1,9 +1,39 @@
 import React, {Component} from 'react'
+import axios from 'axios';
 
 export default class CreateTodo extends Component {
+  constructor() {
+    super()
+    this.state = {
+      taskName: '',
+      assignee: '',
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState(
+        {[event.target.name]: event.target.value}
+    )
+  }
+  async handleSubmit(event) {
+    event.preventDefault()
+    console.log(this.state);
+    const res = await axios.post('/api/todos', {taskName: this.state.taskName, assignee: this.state.assignee});
+    console.log(res.data);
+  }
+
+
   render () {
     return (
-      <form></form>
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor='taskName'>Task Name</label>
+        <input type='text' name='taskName' value={this.state.taskName} onChange={this.handleChange}/>
+        <label htmlFor='assignee'>Assign To</label>
+        <input type='text' name='assignee' value={this.state.assignee} onChange={this.handleChange}/>
+        <button type='submit'>Submit</button>
+      </form>
+
     )
   }
 }
